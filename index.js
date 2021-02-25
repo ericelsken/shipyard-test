5d792971a135a082c74921843fd34733abd6e2c5
Eric Elsken <eric.elsken@gmail.com>
2021-02-25 12:58:52 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(process.env);
+
+var path = require('path');
+console.log(path.join('a', 'b'));
+
+var react = require('react');
+console.log(react);
+
+var invariant = require('invariant');
+console.log(invariant);
